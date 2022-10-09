6a407b67de658007a493094f918d235311252b68
venkatadri123 <venkatadri@experience.com>
2022-10-09 11:17:46 +0530

    added sample code

---
+++ vectors.R
@@ -0,0 +1,21 @@
+# variable intialization ex: var<-value, value>-var, var=val
+new_vector = c(1,2,3,4,5,6)
+
+print(new_vector)
+
+# for loop iteration with condition
+for (i in new_vector) {
+    if (i>2){
+        print(i)
+    } else {
+        next
+    }
+ }
+
+#While loop iteration
+n=0
+while (n<5) {
+    print(n)
+    n=n+1
+ }
+
